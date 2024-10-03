94f531af43a016b0b59f718ad95ba921e24ee530
username-jules <perfinanjuliaalessandra@gmail.com>
2024-10-03 23:38:20 +0800

    :dog:

--- StructurePlease/src/main/java/mainarea/structureplease/MainPageController.java
+++ StructurePlease/src/main/java/mainarea/structureplease/MainPageController.java
@@ -3,6 +3,8 @@ package mainarea.structureplease;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 import java.io.IOException;
 
@@ -11,6 +13,9 @@ public class MainPageController {
     public Node wordID;
     public Node dictionaryID;
     public Node translationID;
+    public Node chabaFlashID;
+    private ImageView wordpopIMGLink, dictionaryIMGLink, translatorIMGLink, chabaflashIMGLink, chabiNormalIMGLink;
+    private Image wordpopIMG, dictionaryIMG, translatorIMG, chabaflashIMG, chabiNormalIMG;
 
     private OpeningController openingController;
 
@@ -26,17 +31,45 @@ public class MainPageController {
         wordID.setVisible(true);
         dictionaryID.setVisible(false);
         translationID.setVisible(false);
+        chabaFlashID.setVisible(false);
     }
 
     public void showDictionary() {
         wordID.setVisible(false);
         dictionaryID.setVisible(true);
         translationID.setVisible(false);
+        chabaFlashID.setVisible(false);
     }
 
     public void showTranslation() {
         wordID.setVisible(false);
         dictionaryID.setVisible(false);
         translationID.setVisible(true);
+        chabaFlashID.setVisible(false);
+
+    }
+
+    public void showChabaFlash() {
+        chabaFlashID.setVisible(true);
+        wordID.setVisible(false);
+        dictionaryID.setVisible(false);
+        translationID.setVisible(false);
+    }
+
+    public void initializingImgs(){
+        wordpopIMG = new Image(getClass().getResourceAsStream("/resources/images/wordpopimg.png"));
+        wordpopIMGLink.setImage(wordpopIMG);
+
+        dictionaryIMG = new Image(getClass().getResourceAsStream("/resources/images/dictionaryimg.png"));
+        dictionaryIMGLink.setImage(dictionaryIMG);
+
+        translatorIMG = new Image(getClass().getResourceAsStream("/resources/images/translatorimg.png"));
+        translatorIMGLink.setImage(translatorIMG);
+
+        chabaflashIMG = new Image(getClass().getResourceAsStream("/resources/images/chabaflashimg.png"));
+        chabaflashIMGLink.setImage(chabaflashIMG);
+
+        chabiNormalIMG = new Image(getClass().getResourceAsStream("/resources/images/chabiNormal.png"));
+        chabiNormalIMGLink.setImage(chabiNormalIMG);
     }
 }
