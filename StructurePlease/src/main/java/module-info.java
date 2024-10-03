34e8638cb3e27efe0da67966a52b57aa8b93bf05
username-jules <perfinanjuliaalessandra@gmail.com>
2024-10-03 21:35:53 +0800

    Add files via upload

---
+++ StructurePlease/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module mainarea.structureplease {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires jdk.compiler;
+
+    opens mainarea.structureplease to javafx.fxml;
+    exports mainarea.structureplease;
+}
